4d3d5b2062ad9b91f79825efe7876c46ab4add1a
belovetech <belovetech@gmail.com>
2022-10-23 21:18:13 +0100

    [Fix]: pycodestyle error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,23 +8,26 @@ from flask import jsonify
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'])
 def get_all_states():
     """Retrieves all state objects"""
     all_states = storage.all(State)
     all_states = [obj.to_dict() for obj in all_states.values()]
     return jsonify(all_states)
-    
+
+
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """Retrieves state objects by id"""
     try:
         state = storage.get(State, state_id)
         state = state.to_dict()
-    except:
+    except Exception:
         abort(404)
-    
+
     return jsonify(state)
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -33,11 +36,12 @@ def delete_state(state_id):
         state = storage.get(State, state_id)
         storage.delete(state)
         state.save()
-    except:
+    except Exception:
         abort(404)
-        
+
     return jsonify({})
-    
+
+
 @app_views.route('/states', methods=['POST'])
 def create_state():
     """Create a state object"""
@@ -46,12 +50,13 @@ def create_state():
         abort(404, 'Not a JSON')
     if 'name' not in data:
         abort(404, 'Missing name')
-        
+
     state = State(**data)
     state.save()
-    
+
     return jsonify(state.to_dict()), 201
-    
+
+
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
     """update state by id"""
@@ -61,7 +66,7 @@ def update_state(state_id):
         abort(404)
     if data is None or not request.is_json:
         abort(404, 'Not a JSON')
-    
+
     for key, value in data.items():
         if key == 'id' or key == 'created_at' or key == 'updated_at':
             continue
@@ -69,5 +74,3 @@ def update_state(state_id):
             setattr(state, key, value)
     state.save()
     return jsonify(state.to_dict())
-            
-            
