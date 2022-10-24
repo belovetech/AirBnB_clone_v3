8e1a3967c0d0792c5679cdac01234eda7eda29b2
belovetech <belovetech@gmail.com>
2022-10-24 08:19:45 +0100

    [Refactor]: refactor code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,6 +9,7 @@ from models import storage
 from models.base_model import BaseModel
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """Retrieves all state objects"""
@@ -16,14 +17,22 @@ def get_all_states():
     all_states = [obj.to_dict() for obj in all_states.values()]
     return jsonify(all_states)
 
-
 # @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-# def get_state(state_id):
-#     """Retrieves state objects by id"""
+# def one_state(state_id):
+#     """Get a single state"""
 #     state = storage.get(State, state_id)
-#     if state is None:
-#         abort(404, 'Not found')
+#     if not state:
+#         abort(404)
 #     return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves state objects by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404, 'Not found')
+    return jsonify(state.to_dict())
 
 
 # @app_views.route('/states/<state_id>', methods=['DELETE'],
@@ -81,15 +90,6 @@ def get_all_states():
 #     for state in states.values():
 #         statess.append(state.to_dict())
 #     return jsonify(statess)
-
-
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def one_state(state_id):
-    """Get a single state"""
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>',
