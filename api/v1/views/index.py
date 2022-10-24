1988794d87f7c0e14a741628f3a272f08d86e95e
ayobamy <ayobamyahmed@gmail.com>
2022-10-24 20:54:23 +0000

    [feat]: updated files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@ return status of the page
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -21,16 +15,3 @@ def get_status():
         'status': 'OK'
     })
 
-
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """retrieves the number of each objects by type"""
-
-    return jsonify({
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User),
-    })
