99ba2a349985d25c1615688a153699da0af4c4db
belovetech <belovetech@gmail.com>
2022-10-24 10:50:37 +0100

    [Update]: remove try and except block

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,7 +16,7 @@ def get_all_cities(state_id):
     """Retrieves all cities in a state"""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404, 'Not found')
+        abort(404)
     # all_cities = storage.all(City)
     # cities = [city.to_dict() for city in all_cities.values()
     # if city.state_id == state_id]
@@ -29,7 +29,7 @@ def get_city(city_id):
     """Retrieve city"""
     city = storage.get(City, city_id)
     if not city:
-        abort(404, 'Not found')
+        abort(404)
     return jsonify(city.to_dict())
 
 
@@ -38,10 +38,10 @@ def delete_city(city_id):
     """Deletes a City object"""
     city = storage.get(City, city_id)
     if not city:
-        abort(404, 'Not found')
+        abort(404)
     city.delete()
     storage.save()
-    return jsonify({})
+    return make_response({}, 200)
 
 
 @app_views.route('/states/<state_id>/cities',
@@ -55,7 +55,7 @@ def create_city(state_id):
         abort(404, 'Missing name')
     state = storage.get(State, state_id)
     if not state:
-        abort(404, 'Not found')
+        abort(404)
 
     new_city = City(**data)
     setattr(new_city, 'state_id', state_id)
@@ -67,17 +67,16 @@ def create_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """Update a city object"""
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404, 'Not found')
     data = request.get_json()
     if not data:
         abort(404, 'Not a JSON')
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404, 'Not found')
 
     ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(city, key, value)
     storage.save()
-
     return make_response(city.to_dict(), 200)
